c0f1ac1a4bca14098c33c9de47f57ee3d77fcd48
Petr Karmashev <smonkl@bk.ru>
2017-12-08 03:13:59 +0300

    Translation entity added

---
+++ src/Entity/Translation.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * @ApiResource
+ * @ORM\Entity(repositoryClass="App\Repository\TranslationRepository")
+ * @ORM\Table(name="translations")
+ */
+class Translation
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     */
+    private $key;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $language;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $translation;
+
+    public function __construct()
+    {
+        $this->id = Uuid::uuid4();
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    public function getTranslation(): string
+    {
+        return $this->translation;
+    }
+
+    public function setKey(string $key): void
+    {
+        $this->key = $key;
+    }
+
+    public function setLanguage(string $language): void
+    {
+        $this->language = $language;
+    }
+
+    public function setTranslation(string $translation): void
+    {
+        $this->translation = $translation;
+    }
+}
---
+++ src/Repository/TranslationRepository.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Repository;
+
+use App\Entity\Translation;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class TranslationRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Translation::class);
+    }
+}
